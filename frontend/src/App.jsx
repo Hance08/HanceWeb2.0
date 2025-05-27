b1f5716aa24e30e449637bf5fc503ef290ac64f4
Hance08 <hancechin08@gmail.com>
2025-05-28 00:31:31 +0800

    重構 App.jsx，整合 NavigationBar 組件並調整路由結構，新增 Admin 相關頁面以改善管理後台的可維護性與使用者體驗。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,21 @@
-import React, { useCallback } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import { AuthProvider, useAuth } from './contexts/AuthContext'
-import { library } from '@fortawesome/fontawesome-svg-core'
-  import { fas } from '@fortawesome/free-solid-svg-icons'
-  import { fab } from '@fortawesome/free-brands-svg-icons'
-  library.add(fas, fab)
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import "./App.css";
 
-import Home from './pages/Home'
-import Login from './pages/Login'
-import ProtectedRoute from './components/ProtectedRoute'
-import About from './pages/About'
-import Portfolio from './pages/Portfolio'
-import AdminAboutEditor from './pages/adminPages/AdminAboutEditor'; // Ensure this is imported
-import AdminDashboard from './pages/adminPages/AdminDashboard'
-import AdminAddPortfolioItem from './pages/adminPages/AdminAddPortfolioItem';
-import AdminEditPortfolioItem from './pages/adminPages/AdminEditPortfolioItem';
-
-// New Admin Sub-Page Components
-import AdminOverview from './pages/adminPages/AdminOverview';
-import AdminPortfolioEditor from './pages/adminPages/AdminPortfolioEditor';
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import NavigationBar from "./components/public/NavigationBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import AdminOverview from "./pages/adminPages/AdminOverview";
+import AdminDashboard from "./pages/adminPages/AdminDashboard";
+import AdminAboutEditor from "./pages/adminPages/AdminAboutEditor";
+import AdminPortfolioEditor from "./pages/adminPages/AdminPortfolioEditor";
+import AdminAddPortfolioItem from "./pages/adminPages/AdminAddPortfolioItem";
+import AdminEditPortfolioItem from "./pages/adminPages/AdminEditPortfolioItem";
 // import AnimatedShapesBackground from './components/animation/AnimatedShapesBackground';
-
-import './App.css'
-
-// Navigation Bar Component (extracted and modified from HomePage)
-const NavigationBar = () => {
-  const { isAuthenticated, currentUser, logout } = useAuth();
-
-  return (
-    <div className="mainNavOverlay"> {/* Changed from styles.mainNavOverlay */}
-      <nav className="navContainer"> {/* Changed from styles.navContainer */}
-        <Link to="/" className="navLink">Hance Web</Link> {/* Changed from styles.navLink */}
-        <div className="navLinksGroup"> {/* Changed from styles.navLinksGroup */}
-          <Link to="/about" className="navLink">關於我</Link>
-          <Link to="/portfolio" className="navLink">作品集</Link>
-          {isAuthenticated ? (
-            <>
-              <Link to="/admin" className="navLink">管理後台</Link>
-            </>
-          ) : ""
-          }
-        </div>
-      </nav>
-    </div>
-  );
-};
 
 function App() {
   console.log("App component rendering...");
@@ -54,7 +24,7 @@ function App() {
     <AuthProvider>
       {/* <AnimatedShapesBackground /> */}
       <Router>
-        <NavigationBar /> {/* Added NavigationBar here */}
+        <NavigationBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -62,12 +32,18 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} />
 
           {/* Admin Routes - AdminDashboardPage as layout for nested routes */}
-          <Route 
-            path="/admin" 
-            element={ <ProtectedRoute><AdminDashboard /></ProtectedRoute> }
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
           >
-            <Route index element={<AdminOverview />} /> {/* Default for /admin */}
-            <Route path="about" element={<AdminAboutEditor />} /> {/* Changed to AdminAboutPage */}
+            <Route index element={<AdminOverview />} />{" "}
+            {/* Default for /admin */}
+            <Route path="about" element={<AdminAboutEditor />} />{" "}
+            {/* Changed to AdminAboutPage */}
             <Route path="portfolio" element={<AdminPortfolioEditor />} />
             {/* 
               The following routes for portfolio item management might also be nested 
@@ -77,36 +53,27 @@ function App() {
           </Route>
 
           {/* Kept for now, consider nesting under /admin/portfolio or a dedicated portfolio management layout */}
-          <Route path="/admin/portfolio/new" element={
-            <ProtectedRoute>
-              <AdminAddPortfolioItem />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/admin/portfolio/new"
+            element={
+              <ProtectedRoute>
+                <AdminAddPortfolioItem />
+              </ProtectedRoute>
+            }
+          />
 
-          <Route path="/admin/portfolio/edit/:itemId" element={
-            <ProtectedRoute>
-              <AdminEditPortfolioItem />
-            </ProtectedRoute>
-          } />
-
-          {/* Old admin routes - to be removed as they are now nested */}
-          {/* 
-          <Route path="/admin/about" element={
-            <ProtectedRoute>
-              <AdminAboutPage /> // This was the old, non-nested route
-            </ProtectedRoute>
-          } />
-
-          <Route path="/admin/portfolio" element={
-            <ProtectedRoute>
-              <AdminPortfolioPage />
-            </ProtectedRoute>
-          } />
-          */}
+          <Route
+            path="/admin/portfolio/edit/:itemId"
+            element={
+              <ProtectedRoute>
+                <AdminEditPortfolioItem />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
